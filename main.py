b6333f1450885225af7876d7185c93d7ab91275e
Mahyar Yaghmaee <mahyar.yaghmaee@gmail.com>
2018-12-05 17:44:30 -0800

    add samople home and about pages

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/home")
+def home():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/about")
+def about():
+    return "<h1>About page!</h1>"
 
 
 if __name__ == '__main__':
