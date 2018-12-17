41c4e1906e4a3caeb058615d1231a1519a4d4723
Mahyar Yaghmaee <mahyar.yaghmaee@gmail.com>
2018-12-17 15:40:47 -0800

    add hyperlinks to new pages

--- main.py
+++ main.py
@@ -13,5 +13,35 @@ def about():
     return render_template('about.html', title='About')
 
 
+@app.route("/artwork")
+def artwork():
+    return render_template('artwork.html', title='ArtWork')
+
+
+@app.route("/news")
+def news():
+    return render_template('news.html', title='News')
+
+
+@app.route("/publications")
+def publications():
+    return render_template('publications.html', title='Publications')
+
+
+@app.route("/services")
+def services():
+    return render_template('services.html', title='Services')
+
+
+@app.route("/forum")
+def forum():
+    return render_template('forum.html', title='Forum')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html', title='Contact')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
